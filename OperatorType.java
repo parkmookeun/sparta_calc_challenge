6fd47f53ba02f9596a6c6d386c4e536645d82e0e
parkmookeun <sohappynow12@naver.com>
2024-10-21 13:56:39 +0900

    refactor : Enum 타입의 매개변수 활용 Enum 타입의 값을 이용하여 전체적인 코드의 가시성을 높였습니다.

--- OperatorType.java
+++ OperatorType.java
@@ -1,8 +1,32 @@
 package sparta.challenge;
 
 public enum OperatorType {
-    ADD,
-    SUBTRACT,
-    MULTIPLY,
-    DIVIDE;
+    ADD("+"),
+    SUBTRACT("-"),
+    MULTIPLY("*"),
+    DIVIDE("/");
+
+    private final String operator;
+
+    OperatorType(String operator) {
+        this.operator = operator;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    /*
+    문자열을 입력ㅎ받아 그에 해당하는 enum을 리턴.
+    @param operator 연산자 기호
+    @return 해당하는 enum
+     */
+    public static OperatorType fromValue(String operator){
+        for(OperatorType type : OperatorType.values()){
+            if(type.getOperator().equals(operator)){
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("올바르지 않은 연산자 입니다.");
+    }
 }
